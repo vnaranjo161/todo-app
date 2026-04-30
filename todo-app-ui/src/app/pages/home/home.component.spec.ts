68007348b0e55a161cedb25edd69dca0895def5e
vnaranjo161 <126474850+vnaranjo161@users.noreply.github.com>
2026-04-29 22:42:18 -0500

    feat((TDA-03.2) - HU(TDA-03)): ✅ test implementation

--- todo-app-ui/src/app/pages/home/home.component.spec.ts
+++ todo-app-ui/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,9 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+
+@Component({ selector: 'app-navbar', template: '', standalone: true })
+class NavbarStub {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +11,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent],
+    }).overrideComponent(HomeComponent, {
+      set: { imports: [NavbarStub] },
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
