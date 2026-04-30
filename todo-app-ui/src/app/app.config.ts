f04c894916719229659172b3cbc7def6002b13eb
vnaranjo161 <126474850+vnaranjo161@users.noreply.github.com>
2026-04-30 12:09:43 -0500

    fix((TDA-07.1) - HU(TDA-07)): 🐛 implementation of auth interceptor

--- todo-app-ui/src/app/app.config.ts
+++ todo-app-ui/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { authInterceptor } from './core/utils/interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
 
@@ -8,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
